93e46e3edf90b0506578b856755e976b86ab9046
Geoffrey-Merville <gmerville1@gmail.com>
2023-12-21 12:37:13 +0100

    Finished the content structure of the articles

--- src/components/Experience/Article.js
+++ src/components/Experience/Article.js
@@ -10,20 +10,24 @@ const Article = ({
   articleEndDate,
   articleDescription,
 }) => {
+  const cleanTitle = DOMPurify.sanitize(articleTitle);
   const cleanEndDate = DOMPurify.sanitize(articleEndDate);
   const cleanDescription = DOMPurify.sanitize(articleDescription);
 
   return (
     <article id={articleId} className="timeline-content scroll-id">
       <hgroup>
-        <h2>{articleTitle}</h2>
+        <h2 dangerouslySetInnerHTML={{__html: cleanTitle}} />
         <p className="location">
-          <strong>{articleCompany}</strong> |
-          <time dateTime={articleStartDateValue}>{articleStartDate}</time> -
-          <time dateTime={articleEndDateValue} dangerouslySetInnerHTML={{__html: cleanEndDate}} />
+          <strong>{articleCompany}</strong>
+          {articleStartDate !== "" ? <time dateTime={articleStartDateValue}> | {articleStartDate} - </time> : ""}
+          {articleEndDate !== "" ? <time
+            dateTime={articleEndDateValue}
+            dangerouslySetInnerHTML={{ __html: cleanEndDate }}
+          /> : ""}
         </p>
       </hgroup>
-      <p dangerouslySetInnerHTML={{__html: cleanDescription}} />
+      <p dangerouslySetInnerHTML={{ __html: cleanDescription }} />
       <footer></footer>
     </article>
   );
