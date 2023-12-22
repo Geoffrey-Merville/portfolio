25f58ea53d439a505f189c4caf7629c684018825
Geoffrey-Merville <gmerville1@gmail.com>
2023-12-22 11:41:27 +0100

    Completed and fixed the skills table layout & styles

--- src/components/Experience/Skills.js
+++ src/components/Experience/Skills.js
@@ -1,41 +1,61 @@
-import SkillsData from '../data/skills-data.json';
+import React from "react";
+import SkillsData from "../data/skills-data.json";
 
 const Skills = () => {
   return (
     <section>
-        <table id="skills">
-            <thead>
-                <tr>
-                    <th scope="col">Type</th>
-                    <th scope="col">Nom</th>
-                    <th scope="col">Niveau Acquis</th>
-                    <th scope="col">Expérience(s) Rattachée(s)</th>
-                </tr>
-            </thead>
-            <tbody>
-              {Object.keys(SkillsData).map((category) => (
-                SkillsData[category].map((skill, index) => (
+      <table id="skills">
+        <thead>
+          <tr>
+            <th scope="col">Type</th>
+            <th scope="col">Nom</th>
+            <th scope="col">Niveau Acquis</th>
+            <th scope="col">Expérience(s) Rattachée(s)</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Object.keys(SkillsData).map((category) =>
+            SkillsData[category].map((skill, index) => (
+              <React.Fragment key={index}>
+                {skill.spacers ? (
+                  <tr className="empty-cell">
+                    <th colSpan='4'></th>
+                  </tr>
+                ) : (
                   <tr key={index}>
-                    {index === 0 && <th rowSpan={SkillsData[category].length} scope="rowgroup">{category}</th>}
-                    <th scope="row"><span title={skill.nom}>{skill.nom}</span></th>
+                    {index === 0 && (
+                      <th rowSpan={SkillsData[category].length} scope="rowgroup">
+                        {category}
+                      </th>
+                    )}
+                    <th scope="row">
+                      <span title={skill.nom}>{skill.nom}</span>
+                    </th>
                     <td>{skill.niveau}</td>
                     <td>
-                      <ul>
+                      {skill.experiences ? <ul>
                         {skill.experiences.map((experience, expIndex) => (
                           <li key={expIndex}>
-                            <a className="linked-experience" href={`#${experience}`}>{experience}</a>
+                            <a
+                              className="linked-experience"
+                              href={`#${experience}`}
+                            >
+                              {experience}
+                            </a>
                           </li>
                         ))}
-                      </ul>
+                        </ul> : ""}
                     </td>
                   </tr>
-                ))
-              ))}
-            </tbody>
-        </table>
-        <footer></footer>
+                )}
+              </React.Fragment>
+            ))
+          )}
+        </tbody>
+      </table>
+      <footer></footer>
     </section>
-  )
-}
+  );
+};
 
 export default Skills;
